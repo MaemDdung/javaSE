341f025f6d55b47fe965aeb84743ec25f26d4f04
admin <admin@COMICOM-V0D98OK>
2022-11-29 21:07:41 +0900

    day07

--- src/day07/mypac/M4A1Weapon.java
+++ src/day07/mypac/M4A1Weapon.java
@@ -1,5 +1,15 @@
 package day07.mypac;
 
-public class M4A1Weapon in {
-	
+public class M4A1Weapon implements Weapon {
+
+	@Override
+	public void attack() {
+		System.out.println("지상 공격을 해요!");
+	}
+
+	@Override
+	public void attackSky() {
+		System.out.println("하늘 공격을 해요!"); 		
+	}
+
 }
